e0a8a0475c8d96aa0bc3286b8d1101bc1cb28b2e
KelemenOzseb <kelemenozzseb@gmail.com>
2026-02-14 20:23:05 +0100

    CRUD logic added

--- Backend/Entity/Dtos/Camel/CreateCamelDto.cs
+++ Backend/Entity/Dtos/Camel/CreateCamelDto.cs
@@ -7,5 +7,8 @@ namespace Entity.Dtos.Camel
     public class CreateCamelDto
     {
         public string Name { get; set; }
+        public string? Color { get; set; }
+        public int? HumpCount { get; set; }
+        public DateTime? LastFed { get; set; }
     }
 }
--- Backend/Logic/Logic/CamelLogic.cs
+++ Backend/Logic/Logic/CamelLogic.cs
@@ -1,6 +1,8 @@
 ﻿using Data.Repository;
+using Entity.Dtos.Camel;
 using Entity.Models;
 using Logic.Helper;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -16,6 +18,69 @@ namespace Logic.Logic
         {
             _repository = repository;
             _dtoProvider = dtoProvider;
+        }
+
+        public async Task<Camel> CreateCamel(CreateCamelDto createCamelDto)
+        {
+            if (createCamelDto.Name == null)
+            {
+                throw new ArgumentNullException("Name can not be null");
+            }
+            if (createCamelDto.HumpCount != null && (createCamelDto.HumpCount > 2 || createCamelDto.HumpCount < 1))
+            {
+                throw new ArgumentException("Hump count must be between 1 and 2");
+            }
+            if (createCamelDto.LastFed != null && createCamelDto.LastFed > DateTime.Now)
+            {
+                throw new ArgumentException("Last fed date cannot be in the future");
+            }
+            var camel = _dtoProvider.Mapper.Map<Camel>(createCamelDto);
+            return await _repository.Create(camel);
+        }
+
+        public async Task DeleteCamel(string id)
+        {
+            var camel = await _repository.GetOne(id);
+            if (camel == null)
+            {
+                throw new KeyNotFoundException("Camel not found");
+            }
+            await _repository.DeleteById(id);
+        }
+
+        public async Task<Camel> UpdateCamel(UpdateCamelDto updateCamelDto, string id)
+        {
+            var camel = await _repository.GetOne(id);
+            if (camel == null)
+            {
+                throw new KeyNotFoundException("Camel not found");
+            }
+            if (updateCamelDto.HumpCount != null && (updateCamelDto.HumpCount > 2 || updateCamelDto.HumpCount < 1))
+            {
+                throw new ArgumentException("Hump count must be between 1 and 2");
+            }
+            if (updateCamelDto.LastFed != null && updateCamelDto.LastFed > DateTime.Now)
+            {
+                throw new ArgumentException("Last fed date cannot be in the future");
+            }
+            camel = _dtoProvider.Mapper.Map(updateCamelDto, camel);
+            return await _repository.Update(camel);
+        }
+
+        public async Task<Camel> GetCamel(string id)
+        {
+            var camel = await _repository.GetOne(id);
+            if (camel == null)
+            {
+                throw new KeyNotFoundException("Camel not found");
+            }
+            return camel;
+        }
+
+        public async Task<IEnumerable<Camel>> GetAllCamels()
+        {
+            var camels = await _repository.GetAll().ToListAsync();
+            return camels;
         }
     }
 }
