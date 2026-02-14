82d69893eda35d41275de5b55b45dbb8160c794a
KelemenOzseb <kelemenozzseb@gmail.com>
2026-02-14 17:13:20 +0100

    model done

---
+++ Backend/Entity/Helper/IIdentity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Entity.Helper
+{
+    public interface IIdentity
+    {
+        string Id { get; set; }
+    }
+}
---
+++ Backend/Entity/Models/Camel.cs
@@ -0,0 +1,22 @@
+﻿using Entity.Helper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entity.Models
+{
+    public class Camel : IIdentity
+    {
+        public Camel(string name)
+        {
+            Id = Guid.NewGuid().ToString();
+            Name = name;
+        }
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int HumpCount { get; set; }
+        public DateTime LastFed { get; set; }
+    }
+}
