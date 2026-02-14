5a00cce84526e723cd69f8ac3fe467335e41a9b9
KelemenOzseb <kelemenozzseb@gmail.com>
2026-02-14 17:45:16 +0100

    db context added

---
+++ Backend/Data/CamelRegistryDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Entity.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data
+{
+    public class CamelRegistryDbContext: DbContext
+    {
+        public CamelRegistryDbContext(DbContextOptions<CamelRegistryDbContext> options) : base(options) {}
+        public DbSet<Camel> Camels { get; set; }
+    }
+}
