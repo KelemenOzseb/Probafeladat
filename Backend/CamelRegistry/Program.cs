7930e0a95e8aa67ebbf34defa259a8e837d8dab1
KelemenOzseb <kelemenozzseb@gmail.com>
2026-02-14 20:51:09 +0100

    basic apis done

--- Backend/CamelRegistry/Program.cs
+++ Backend/CamelRegistry/Program.cs
@@ -1,5 +1,7 @@
 
 using Data;
+using Entity.Dtos.Camel;
+using Logic.Logic;
 using Microsoft.EntityFrameworkCore;
 
 namespace CamelRegistry
@@ -25,6 +27,71 @@ namespace CamelRegistry
                 var dbContext = scope.ServiceProvider.GetRequiredService<CamelRegistryDbContext>();
                 dbContext.Database.Migrate();
             }
+
+            // Endpoints
+            // 1.  Új teve létrehozása (POST) 
+            app.MapPost("/api/camels", async (CreateCamelDto dto, CamelLogic logic) =>
+            {
+                try
+                {
+                    var newCamel = await logic.CreateCamel(dto);
+                    return Results.Created($"/api/camels/{newCamel.Id}", newCamel);
+                }
+                catch (ArgumentException ex)
+                {
+                    return Results.BadRequest(ex.Message);
+                }
+            });
+
+            // 2. Tevék listázása (GET) 
+            app.MapGet("/api/camels", async (CamelLogic logic) =>
+                Results.Ok(await logic.GetAllCamels()));
+
+            // 3. Egy adott teve lekérdezése (GET /{id}) 
+            app.MapGet("/api/camels/{id}", async (string id, CamelLogic logic) =>
+            {
+                try
+                {
+                    var camel = await logic.GetCamel(id);
+                    return Results.Ok(camel);
+                }
+                catch (KeyNotFoundException)
+                {
+                    return Results.NotFound();
+                }
+            });
+
+            // 4. Teve adatainak módosítása (PUT vagy PATCH /{id}) (Esetemben PUT)
+            app.MapPut("/api/camels/{id}", async (string id, UpdateCamelDto dto, CamelLogic logic) =>
+            {
+                try
+                {
+                    var updatedCamel = await logic.UpdateCamel(dto, id);
+                    return Results.Ok(updatedCamel);
+                }
+                catch (KeyNotFoundException)
+                {
+                    return Results.NotFound();
+                }
+                catch (ArgumentException ex)
+                {
+                    return Results.BadRequest(ex.Message);
+                }
+            });
+
+            // 5. Teve törlése (DELETE /{id}) 
+            app.MapDelete("/api/camels/{id}", async (string id, CamelLogic logic) =>
+            {
+                try
+                {
+                    await logic.DeleteCamel(id);
+                    return Results.NoContent();
+                }
+                catch (KeyNotFoundException)
+                {
+                    return Results.NotFound();
+                }
+            });
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
