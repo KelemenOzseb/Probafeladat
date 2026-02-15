9b611e5f6ea7378cf4fa0d9360ed9f7c1f174ced
KelemenOzseb <kelemenozzseb@gmail.com>
2026-02-15 19:32:30 +0100

    service done

--- Frontend/src/app/models/camel.ts
+++ Frontend/src/app/models/camel.ts
@@ -1,2 +1,7 @@
 export class Camel {
+    id: string = ""
+    name: string = ""
+    color: string | null = null
+    humpCount: number | null = null
+    lastFed: Date | null = null
 }
--- Frontend/src/app/services/camel-service.ts
+++ Frontend/src/app/services/camel-service.ts
@@ -1,8 +1,40 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { BehaviorSubject } from 'rxjs';
+import { Camel } from '../models/camel';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CamelService {
-  
+  backendUrl = environment.backendUrl + 'camels'
+  camels = new BehaviorSubject<Camel[]>([])
+  camels$ = this.camels.asObservable()
+
+  constructor(private http:HttpClient){}
+
+  getCamels(): void {
+    this.http.get<Camel[]>(this.backendUrl).subscribe(res =>{
+        this.camels.next(res)
+    }
+    )
+  }
+
+  addCamel(body: Camel): void {
+    this.http.post<Camel>(this.backendUrl, body).subscribe(res => {
+        const current = this.camels.value
+        this.camels.next([...current, res])
+    }
+    )
+  }
+
+  updateCamel(body: Camel): void {
+    this.http.put<Camel>(this.backendUrl + `${body.id}`, body).subscribe({
+      next: () => {
+        this.getCamels()
+      }
+    }
+    )
+  }
 }
